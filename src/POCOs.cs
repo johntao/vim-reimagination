e05b2f65e093a2565765a6ee0b760b2d933e8f0d
John Tao <zytao24@gmail.com>
2024-05-11 22:42:57 +0800

    refactor: refine MotionV2 algorithm act 2

--- src/POCOs.cs
+++ src/POCOs.cs
@@ -27,9 +27,11 @@ public ref struct Buffer1D
   private readonly ReadOnlySpan<char> _secondary = ",.:+=-*/\\(){}[]<>!@#$%^&*;\"'`~|?";
   private readonly ReadOnlySpan<char> _space = " \t\n\r\f\v";
   private readonly SearchValues<char> _searchPrimary, _searchSecondary, _searchSpace;
-  private int _cursor1D, _left2D, _top2D;
+  private int _cursor1D;
+  private int _left2D;
+  private int _top2D;
+  private Direction _direction;
   private CharKind _prev;
-  internal readonly int Cursor1D => _cursor1D;
   internal readonly int Left2D => _left2D;
   internal readonly int Top2D => _top2D;
   public Buffer1D(ReadOnlySpan<char> Content, int Width)
@@ -49,66 +51,73 @@ public ref struct Buffer1D
     var c when _searchSpace.Contains(c) => CharKind.Space,
     _ => CharKind.None,
   };
-  public bool MoveNext()
-  {
-    var hasNext = _cursor1D + 1 < Content.Length;
-    if (hasNext)
-    {
-      ++_cursor1D;
-      ++_left2D;
-      _prev = Current;
-    }
-    return hasNext;
-  }
-
-  public bool FetchNext()
-  {
-    var hasNext = _cursor1D + 1 < Content.Length;
-    if (hasNext)
-    {
-      ++_cursor1D;
-      ++_left2D;
-      var hasNextAndIsSameKind = Current == _prev;
-      _prev = Current;
-      return hasNextAndIsSameKind;
-    }
-    return false;
-  }
-  public bool FetchPrev()
-  {
-    var hasNext = _cursor1D - 1 >= 0;
-    if (hasNext)
-    {
-      --_cursor1D;
-      --_left2D;
-      var hasNextAndIsSameKind = Current == _prev;
-      _prev = Current;
-      return hasNextAndIsSameKind;
-    }
-    return hasNext;
-  }
-
-  public bool MovePrev()
-  {
-    var hasNext = _cursor1D - 1 >= 0;
-    if (hasNext)
-    {
-      --_cursor1D;
-      --_left2D;
-      _prev = Current;
-    }
-    return hasNext;
-  }
-
-  public void Reset(int cursor1D)
-  {
-    _cursor1D = cursor1D;
-  }
-  public void Reset(int left2D, int top2D)
+  public void Reset(int left2D, int top2D, Direction direction)
   {
     _left2D = left2D;
     _top2D = top2D;
     _cursor1D = top2D * Width + left2D;
     _prev = Current;
+    _direction = direction;
+  }
+  public readonly bool HasNext() => _direction switch
+  {
+    Direction.Forward => _cursor1D + 1 < Content.Length,
+    Direction.Backward => _cursor1D - 1 >= 0,
+    _ => throw new NotImplementedException()
+  };
+  public bool Move()
+  {
+    if (_direction == Direction.Forward)
+    {
+      var hasNext = _cursor1D + 1 < Content.Length;
+      if (hasNext)
+      {
+        ++_cursor1D;
+        ++_left2D;
+        _prev = Current;
+      }
+      return hasNext;
+    }
+    else
+    {
+      var hasNext = _cursor1D - 1 >= 0;
+      if (hasNext)
+      {
+        --_cursor1D;
+        --_left2D;
+        _prev = Current;
+      }
+      return hasNext;
+    }
+  }
+  public bool MoveCheck()
+  {
+    if (_direction == Direction.Forward)
+    {
+      var hasNext = _cursor1D + 1 < Content.Length;
+      if (hasNext)
+      {
+        ++_cursor1D;
+        ++_left2D;
+        var hasNextAndIsSameKind = Current == _prev;
+        _prev = Current;
+        // Console.SetCursorPosition(_left2D, _top2D);
+        return hasNextAndIsSameKind;
+      }
+      return hasNext;
+    }
+    else
+    {
+      var hasNext = _cursor1D - 1 >= 0;
+      if (hasNext)
+      {
+        --_cursor1D;
+        --_left2D;
+        var hasNextAndIsSameKind = Current == _prev;
+        _prev = Current;
+        return hasNextAndIsSameKind;
+      }
+      return hasNext;
+    }
   }
 }
--- src/WordMotionV2.cs
+++ src/WordMotionV2.cs
@@ -1,116 +1,71 @@
 namespace VimRenaissance;
+public enum Direction
+{
+  Forward,
+  Backward,
+}
 internal class WordMotionV2 : IWordMotion
 {
   public (int, int) GetSmallWordEndForward(int left2D, int top2D, Buffer1D buffer)
   {
-    buffer.Reset(left2D, top2D);
-    var prev = buffer.Current;
-    var hasNext = buffer.MoveNext();
-    var current = hasNext ? buffer.Current : CharKind.None;
-    switch ((prev, current))
+    buffer.Reset(left2D, top2D, Direction.Forward);
+    if (!buffer.Move()) return (left2D, top2D);
+    if (buffer.Current == CharKind.Space)
     {
-      case (CharKind.Primary, CharKind.Primary):
-      case (CharKind.Secondary, CharKind.Secondary):
-      case (CharKind.Space, CharKind.Primary):
-      case (CharKind.Space, CharKind.Secondary):
-      case (CharKind.Primary, CharKind.Secondary):
-      case (CharKind.Secondary, CharKind.Primary):
-        {
-          while (buffer.FetchNext()) ;
-          return (buffer.Left2D - 1, buffer.Top2D);
-        }
-      case (_, CharKind.Space):
-        {
-          while (buffer.FetchNext()) ;
-          while (buffer.FetchNext()) ;
-          return (buffer.Left2D - 1, buffer.Top2D);
-        }
-      default:
-        return (left2D, top2D);
+      while (buffer.MoveCheck()) ;
+      while (buffer.MoveCheck()) ;
+      return (buffer.Left2D - 1, buffer.Top2D);
+    }
+    else
+    {
+      while (buffer.MoveCheck()) ;
+      return (buffer.Left2D - 1, buffer.Top2D);
     }
   }
   public (int, int) GetSmallWordBeginForward(int left2D, int top2D, Buffer1D buffer)
   {
-    buffer.Reset(left2D, top2D);
-    var prev = buffer.Current;
-    var hasNext = buffer.MoveNext();
-    var current = hasNext ? buffer.Current : CharKind.None;
-    switch ((prev, current))
+    buffer.Reset(left2D, top2D, Direction.Forward);
+    if (!buffer.HasNext()) return (left2D, top2D);
+    while (buffer.MoveCheck()) ;
+    if (buffer.Current == CharKind.Space)
     {
-      case (_, CharKind.Space):
-        while (buffer.FetchNext()) ;
-        return (buffer.Left2D, buffer.Top2D);
-      case (CharKind.Primary, CharKind.Secondary):
-      case (CharKind.Secondary, CharKind.Primary):
-      case (CharKind.Space, CharKind.Primary):
-      case (CharKind.Space, CharKind.Secondary):
-        return (buffer.Left2D, buffer.Top2D);
-      case (CharKind.Primary, CharKind.Primary):
-      case (CharKind.Secondary, CharKind.Secondary):
-        while (buffer.FetchNext()) ;
-        var newKind = buffer.Current;
-        if (newKind != CharKind.Space) // this is the case of alternative kind
-          return (buffer.Left2D, buffer.Top2D);
-        while (buffer.FetchNext()) ;
-        return (buffer.Left2D, buffer.Top2D);
-      default:
-        return (left2D, top2D);
+      while (buffer.MoveCheck()) ;
+      return (buffer.Left2D, buffer.Top2D);
+    }
+    else
+    {
+      return (buffer.Left2D, buffer.Top2D);
     }
   }
   public (int, int) GetSmallWordEndBackward(int left2D, int top2D, Buffer1D buffer)
   {
-    buffer.Reset(left2D, top2D);
-    var prev = buffer.Current;
-    var hasNext = buffer.MovePrev();
-    var current = hasNext ? buffer.Current : CharKind.None;
-    switch ((prev, current))
+    buffer.Reset(left2D, top2D, Direction.Backward);
+    if (!buffer.HasNext()) return (left2D, top2D);
+    while (buffer.MoveCheck()) ;
+    if (buffer.Current == CharKind.Space)
     {
-      case (_, CharKind.Space):
-        while (buffer.FetchPrev()) ;
-        return (buffer.Left2D, buffer.Top2D);
-      case (CharKind.Primary, CharKind.Secondary):
-      case (CharKind.Secondary, CharKind.Primary):
-      case (CharKind.Space, CharKind.Primary):
-      case (CharKind.Space, CharKind.Secondary):
-        return (buffer.Left2D, buffer.Top2D);
-      case (CharKind.Primary, CharKind.Primary):
-      case (CharKind.Secondary, CharKind.Secondary):
-        while (buffer.FetchPrev()) ;
-        var newKind = buffer.Current;
-        if (newKind != CharKind.Space) // this is the case of alternative kind
-          return (buffer.Left2D, buffer.Top2D);
-        while (buffer.FetchPrev()) ;
-        return (buffer.Left2D, buffer.Top2D);
-      default:
-        return (left2D, top2D);
+      while (buffer.MoveCheck()) ;
+      return (buffer.Left2D, buffer.Top2D);
+    }
+    else
+    {
+      return (buffer.Left2D, buffer.Top2D);
     }
   }
   public (int, int) GetSmallWordBeginBackward(int left2D, int top2D, Buffer1D buffer)
   {
-    buffer.Reset(left2D, top2D);
-    var prev = buffer.Current;
-    var hasNext = buffer.MovePrev();
-    var current = hasNext ? buffer.Current : CharKind.None;
-    switch ((prev, current))
+    buffer.Reset(left2D, top2D, Direction.Backward);
+    if (!buffer.Move()) return (left2D, top2D);
+    if (buffer.Current == CharKind.Space)
     {
-      case (CharKind.Primary, CharKind.Primary):
-      case (CharKind.Secondary, CharKind.Secondary):
-      case (CharKind.Space, CharKind.Primary):
-      case (CharKind.Space, CharKind.Secondary):
-      case (CharKind.Primary, CharKind.Secondary):
-      case (CharKind.Secondary, CharKind.Primary):
-        {
-          while (buffer.FetchPrev()) ;
-          return (buffer.Left2D + 1, buffer.Top2D);
-        }
-      case (_, CharKind.Space):
-        {
-          while (buffer.FetchPrev()) ;
-          while (buffer.FetchPrev()) ;
-          return (buffer.Left2D + 1, buffer.Top2D);
-        }
-      default:
-        return (left2D, top2D);
+      while (buffer.MoveCheck()) ;
+      while (buffer.MoveCheck()) ;
+      return (buffer.Left2D + 1, buffer.Top2D);
+    }
+    else
+    {
+      while (buffer.MoveCheck()) ;
+      return (buffer.Left2D + 1, buffer.Top2D);
     }
   }
 }
