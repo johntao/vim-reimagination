469967b5c18b01984437831de3bc6e4ebc88c44e
John Tao <zytao24@gmail.com>
2024-05-12 13:27:48 +0800

    refactor: move types. replace public by internal if possible

--- src/Buffer1D.cs
+++ src/Buffer1D.cs
@@ -1,46 +1,49 @@
-public ref struct Buffer1D
+using System.Buffers;
+namespace VimRenaissance;
+
+internal ref struct Buffer1D
 {
-  private readonly ReadOnlySpan<char> _primary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
-  private readonly ReadOnlySpan<char> _secondary = ",.:+=-*/\\(){}[]<>!@#$%^&*;\"'`~|?";
-  private readonly ReadOnlySpan<char> _space = " \t\n\r\f\v";
   private readonly SearchValues<char> _searchPrimary;
   private readonly SearchValues<char> _searchSecondary;
   private readonly SearchValues<char> _searchSpace;
-  private int _cursor1D;
   internal Cursor2D Cursor2D { get; private set; }
+  private int _cursor1D;
   private Direction _direction;
   private CharKind _prev;
-  public Buffer1D(ReadOnlySpan<char> Content, int Width)
+  internal Buffer1D(ReadOnlySpan<char> Content, int Width)
   {
     this.Content = Content;
     this.Width = Width;
-    _searchPrimary = SearchValues.Create(_primary);
-    _searchSecondary = SearchValues.Create(_secondary);
-    _searchSpace = SearchValues.Create(_space);
+    const string Primary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+    const string Secondary = ",.:+=-*/\\(){}[]<>!@#$%^&*;\"'`~|?";
+    const string Space = " \t\n\r\f\v";
+    _searchPrimary = SearchValues.Create(Primary);
+    _searchSecondary = SearchValues.Create(Secondary);
+    _searchSpace = SearchValues.Create(Space);
   }
   internal readonly ReadOnlySpan<char> Content;
   internal readonly int Width;
-  public readonly CharKind Current => Content[_cursor1D] switch
+  internal readonly CharKind Current => Content[_cursor1D] switch
   {
     var c when _searchPrimary.Contains(c) => CharKind.Primary,
     var c when _searchSecondary.Contains(c) => CharKind.Secondary,
     var c when _searchSpace.Contains(c) => CharKind.Space,
     _ => CharKind.None,
   };
-  public void Reset(int left2D, int top2D, Direction direction)
+  internal void Reset(int left2D, int top2D, Direction direction)
   {
     Cursor2D = new Cursor2D { Left = left2D, Top = top2D };
     _cursor1D = top2D * Width + left2D;
     _prev = Current;
     _direction = direction;
   }
-  public readonly bool HasNext() => _direction switch
+  internal readonly bool HasNext() => _direction switch
   {
     Direction.Forward => _cursor1D + 1 < Content.Length,
     Direction.Backward => _cursor1D - 1 >= 0,
     _ => throw new NotImplementedException()
   };
-  public bool Move(out CharKind charKind)
+  internal bool Move(out CharKind charKind)
   {
     switch (_direction)
     {
@@ -72,7 +75,7 @@ public ref struct Buffer1D
         throw new NotImplementedException();
     }
   }
-  public bool MoveCheck()
+  internal bool MoveCheck()
   {
     switch (_direction)
     {
